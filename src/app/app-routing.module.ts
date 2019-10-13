a5213b5ec6a6540817a951cb650b6eb1ff61a129
Dmitry Ascended <DieMon91@list.ru>
2019-10-13 21:07:11 +0200

    commit #3: Authorization Form created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component'
 import { MainPageComponent } from './main-page/main-page.component'
 import { StuffItemPageComponent } from './stuff-item-page/stuff-item-page.component'
+import { LoginPageComponent } from './login-page/login-page.component'
 
 const routes: Routes = [
 	{
@@ -10,6 +11,7 @@ const routes: Routes = [
 			{path: '', redirectTo: '/', pathMatch: 'full'},
 			{path: '', component: MainPageComponent},
 			{path: 'stuff-item/:id', component: StuffItemPageComponent},
+			{path: 'login', component: LoginPageComponent}
 		]
 	}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,8 @@ import { MainLayoutComponent } from './shared/components/main-layout/main-layout
 import { MainPageComponent } from './main-page/main-page.component';
 import { StuffItemPageComponent } from './stuff-item-page/stuff-item-page.component';
 import { StuffItemComponent } from './shared/components/stuff-item/stuff-item.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { StorePageComponent } from './store-page/store-page.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,14 @@ import { StuffItemComponent } from './shared/components/stuff-item/stuff-item.co
     MainLayoutComponent,
     MainPageComponent,
     StuffItemPageComponent,
-    StuffItemComponent
+    StuffItemComponent,
+    LoginPageComponent,
+    StorePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
