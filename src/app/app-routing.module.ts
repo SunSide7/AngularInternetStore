066a0b55ec9d2023476481a58566272100333820
Dreiko <dreiko@inbox.ru>
2019-10-14 11:40:16 +0200

    commit #5: Auth Guard created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { MainLayoutComponent } from './shared/components/main-layout/main-layout
 import { MainPageComponent } from './main-page/main-page.component'
 import { StuffItemPageComponent } from './stuff-item-page/stuff-item-page.component'
 import { LoginPageComponent } from './login-page/login-page.component'
+import { AuthGuard } from './shared/guards/auth.guard'
 
 const routes: Routes = [
 	{
@@ -11,7 +12,7 @@ const routes: Routes = [
 			{path: '', redirectTo: '/', pathMatch: 'full'},
 			{path: '', component: MainPageComponent},
 			{path: 'stuff-item/:id', component: StuffItemPageComponent},
-			{path: 'login', component: LoginPageComponent}
+			{path: 'login', component: LoginPageComponent, canActivate: [AuthGuard]}
 		]
 	}
 ];
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,12 +1,35 @@
-import { CanActivate, ActivateRouteSnapshot, RouterStateSnapshot } from '@angular/router'
+import { CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router'
+import { Observable } from 'rxjs'
+import { Injectable } from '@angular/core'
+
+import { AuthService } from '../services/auth.service'
+
+@Injectable({providedIn: 'root'})
 
 export class AuthGuard implements CanActivate {
+
+	constructor(
+		private authService: AuthService,
+		private router: Router
+	) {}
 
 	canActivate(
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean> | Promise<boolean> | boolean {
-		return null
+		return this.authService.isAuthenticated().then(isAuth => {
+			if (isAuth) {
+				console.log(isAuth)
+				return true
+			} else {
+				console.log(isAuth)
+				this.router.navigate(['/'], {
+					queryParams: {
+						auth: false
+					}
+				})
+			}
+		})
 	}
 
 }
